291fe18548db70630912b9257eac7d2323e1486a
Hoang Pham <hoangpham@swiftcode.me>
2025-09-01 05:23:04 +0700

    Fix

--- Sources/SwiftcodeMe/BlogTheme/Nodes/Node+Post.swift
+++ Sources/SwiftcodeMe/BlogTheme/Nodes/Node+Post.swift
@@ -29,7 +29,7 @@ extension Node where Context == HTML.BodyContext {
                 .div(
                     .contentBody(item.body)
                 )
-            ), 
+            )
         )
     }
 }
